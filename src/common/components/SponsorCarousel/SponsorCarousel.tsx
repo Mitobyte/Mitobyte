4f47752ab54d11b29d409e8975336071a51a8c34
Joseph Murphy <murphyjoseph@users.noreply.github.com>
2024-09-11 18:56:16 -0500

    fix: needs key

--- src/common/components/SponsorCarousel/SponsorCarousel.tsx
+++ src/common/components/SponsorCarousel/SponsorCarousel.tsx
@@ -65,6 +65,7 @@ const InnerCarousel = () => {
             alt="sponsor"
             objectFit="contain"
             src={sponsor.logo}
+            key={sponsor.name}
           />
         ))}
       </Box>
