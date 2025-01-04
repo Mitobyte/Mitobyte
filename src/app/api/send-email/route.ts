6e31dd8b2e739f096df08b34826514262a6ba165
alwick <awick@reinventauctions.com>
2025-01-04 15:12:17 -0600

    Deployment fix.

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer";
 import { NextResponse, NextRequest } from "next/server";
 
+export const runtime = "edge";
+
 export async function POST(req: NextRequest) {
   try {
     // Parse the request body to extract form data
